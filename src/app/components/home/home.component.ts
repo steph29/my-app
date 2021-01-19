44f7f4ca097bc796eebdc25bf073f6c91b9b53fc
stephane <s.verardo29@gmail.com>
2021-01-19 22:47:19 +0100

    change police, style, background from home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,6 +13,12 @@ import { Subject } from 'rxjs';
 export class HomeComponent implements OnInit {
   cardContainerMenu = [
     {
+      cardTitle: 'Création',
+      cardBody:
+        "Parce que votre site est la première image de vous, celui-ci doit vous ressembler le plus possible. Avoir sa propre identité visuelle ce n'est pas juste une question de goût sur les couleurs. Elle doit faire passer le bon message et marquer les esprits en un seul coup de regard.",
+      cardImage: '/assets/images/nature3.jpg',
+    },
+    {
       cardTitle: 'Sécurité',
       cardBody:
         "La sécurisation des données personnelles est devenue un enjeu majeur pour les utilisateurs et les entreprises lors de leur navigation sur le web. Les infrastructures d'hébergement de nos sites, c'est-à-dire les serveurs, sont optimisés pour une haute sécurité. Nous mettons tout en oeuvre pour permettre un temps de réponse rapide de votre site. (HTTPS, HSTS, CSP, X-XSS...) Nous utilisons tous les outils disponibles pour assurer une fiabilité de performance de votre site. Nous fournissons un audit mensuel sur cette prestation.",
@@ -23,12 +29,6 @@ export class HomeComponent implements OnInit {
       cardBody:
         'Nous développons votre site et utilisons tous les outils à notre disposition pour rendre votre site internet "ULTRA PERFORMANT". Nous travaillons sur l\'optimisation du temps de chargement des pages et sur la sécurité de votre projet. ',
       cardImage: '/assets/images/nature2.jpg',
-    },
-    {
-      cardTitle: 'Création',
-      cardBody:
-        "Parce que votre site est la première image de vous, celui-ci doit vous ressembler le plus possible. Avoir sa propre identité visuelle ce n'est pas juste une question de goût sur les couleurs. Elle doit faire passer le bon message et marquer les esprits en un seul coup de regard.",
-      cardImage: '/assets/images/nature3.jpg',
     },
   ];
   constructor() {}
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -16,7 +16,8 @@ export class ProjectsComponent implements OnInit {
       projectId: 0,
       projectImage: '/assets/images/TEV.png',
       projectTitle: 'Terre En Vie',
-      projectText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      projectText:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum architecto eos culpa labore modi cum veniam sequi a id excepturi maxime quia, cupiditate ex molestiae eius numquam nostrum, delectus neque Explicabo culpa perferendis voluptatum quam tenetur. Cumque, cupiditate aliquam odit amet similique deserunt aperiam fugit harum rem, a quis cum qui dicta voluptatibus officia repellendus vel esse porro doloremque explicabo? Repudiandae saepe qui eum similique nemo in, molestiae unde voluptate! Consequuntur eaque, qui quam, obcaecati provident nostrum nesciunt, perferendis voluptatem ipsum aspernatur eius ipsam tempore. Natus corrupti reiciendis numquam illo. Blanditiis in sequi quo, perferendis, corporis ullam exercitationem magnam laborum necessitatibus ab aliquid voluptatibus, soluta eveniet. Mollitia pariatur, exercitationem illo, quisquam quos laboriosam doloremque eaque cupiditate praesentium a iusto? Maiores.',
       projectUrl: 'https://www.terreenvie.com/benevoles/inscription.php',
       projectColor: '#ffffff',
     },
@@ -24,7 +25,8 @@ export class ProjectsComponent implements OnInit {
       projectId: 1,
       projectImage: '/assets/images/nature1.jpg',
       projectTitle: 'Evaluation 1 Digital Campus Live',
-      projectText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      projectText:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum architecto eos culpa labore modi cum veniam sequi a id excepturi maxime quia, cupiditate ex molestiae eius numquam nostrum, delectus neque Explicabo culpa perferendis voluptatum quam tenetur. Cumque, cupiditate aliquam odit amet similique deserunt aperiam fugit harum rem, a quis cum qui dicta voluptatibus officia repellendus vel esse porro doloremque explicabo? Repudiandae saepe qui eum similique nemo in, molestiae unde voluptate! Consequuntur eaque, qui quam, obcaecati provident nostrum nesciunt, perferendis voluptatem ipsum aspernatur eius ipsam tempore. Natus corrupti reiciendis numquam illo. Blanditiis in sequi quo, perferendis, corporis ullam exercitationem magnam laborum necessitatibus ab aliquid voluptatibus, soluta eveniet. Mollitia pariatur, exercitationem illo, quisquam quos laboriosam doloremque eaque cupiditate praesentium a iusto? Maiores.',
       projectUrl: '#',
       projectColor: '#ececec',
     },
@@ -32,7 +34,8 @@ export class ProjectsComponent implements OnInit {
       projectId: 2,
       projectImage: '/assets/images/nature2.jpg',
       projectTitle: "John d'oeuf: projet Flutter ",
-      projectText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      projectText:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum architecto eos culpa labore modi cum veniam sequi a id excepturi maxime quia, cupiditate ex molestiae eius numquam nostrum, delectus neque Explicabo culpa perferendis voluptatum quam tenetur. Cumque, cupiditate aliquam odit amet similique deserunt aperiam fugit harum rem, a quis cum qui dicta voluptatibus officia repellendus vel esse porro doloremque explicabo? Repudiandae saepe qui eum similique nemo in, molestiae unde voluptate! Consequuntur eaque, qui quam, obcaecati provident nostrum nesciunt, perferendis voluptatem ipsum aspernatur eius ipsam tempore. Natus corrupti reiciendis numquam illo. Blanditiis in sequi quo, perferendis, corporis ullam exercitationem magnam laborum necessitatibus ab aliquid voluptatibus, soluta eveniet. Mollitia pariatur, exercitationem illo, quisquam quos laboriosam doloremque eaque cupiditate praesentium a iusto? Maiores.',
       projectUrl: 'https://apps.apple.com/kn/app/john-doeuf/id1520277206',
       projectColor: '#ffffff',
     },
@@ -40,7 +43,8 @@ export class ProjectsComponent implements OnInit {
       projectId: 3,
       projectImage: '/assets/images/nature3.jpg',
       projectTitle: 'Ma super Agence: projet Symfony',
-      projectText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      projectText:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum architecto eos culpa labore modi cum veniam sequi a id excepturi maxime quia, cupiditate ex molestiae eius numquam nostrum, delectus neque Explicabo culpa perferendis voluptatum quam tenetur. Cumque, cupiditate aliquam odit amet similique deserunt aperiam fugit harum rem, a quis cum qui dicta voluptatibus officia repellendus vel esse porro doloremque explicabo? Repudiandae saepe qui eum similique nemo in, molestiae unde voluptate! Consequuntur eaque, qui quam, obcaecati provident nostrum nesciunt, perferendis voluptatem ipsum aspernatur eius ipsam tempore. Natus corrupti reiciendis numquam illo. Blanditiis in sequi quo, perferendis, corporis ullam exercitationem magnam laborum necessitatibus ab aliquid voluptatibus, soluta eveniet. Mollitia pariatur, exercitationem illo, quisquam quos laboriosam doloremque eaque cupiditate praesentium a iusto? Maiores.',
       projectUrl: '#',
       projectColor: '#ececec',
     },
@@ -48,7 +52,8 @@ export class ProjectsComponent implements OnInit {
       projectId: 4,
       projectImage: '/assets/images/nature4.jpg',
       projectTitle: 'Training Frontend: projet Frontend Mentor',
-      projectText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      projectText:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum architecto eos culpa labore modi cum veniam sequi a id excepturi maxime quia, cupiditate ex molestiae eius numquam nostrum, delectus neque Explicabo culpa perferendis voluptatum quam tenetur. Cumque, cupiditate aliquam odit amet similique deserunt aperiam fugit harum rem, a quis cum qui dicta voluptatibus officia repellendus vel esse porro doloremque explicabo? Repudiandae saepe qui eum similique nemo in, molestiae unde voluptate! Consequuntur eaque, qui quam, obcaecati provident nostrum nesciunt, perferendis voluptatem ipsum aspernatur eius ipsam tempore. Natus corrupti reiciendis numquam illo. Blanditiis in sequi quo, perferendis, corporis ullam exercitationem magnam laborum necessitatibus ab aliquid voluptatibus, soluta eveniet. Mollitia pariatur, exercitationem illo, quisquam quos laboriosam doloremque eaque cupiditate praesentium a iusto? Maiores.',
       projectUrl: '#',
       projectColor: '#ffffff',
     },
