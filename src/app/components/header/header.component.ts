7c6c436c59b1e886549de8b8828b8404c085cbc0
stephane <s.verardo29@gmail.com>
2021-01-19 11:40:22 +0100

    Responsive navbar is done.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+declare var jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-header',
@@ -6,12 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('navBurger') navBurger: ElementRef;
+  @ViewChild('navMenu') navMenu: ElementRef;
+
   menuItems = [
     { linkId: 1, linkName: 'home', linkUrl: '' },
     { linkId: 2, linkName: 'projects', linkUrl: 'projects' },
     { linkId: 3, linkName: 'passions', linkUrl: 'passions' },
-    { linkId: 4, linkName: 'contact', linkUrl: 'contact' },
   ];
+  buttonItems = [{ linkId: 4, linkName: 'contact', linkUrl: 'contact' }];
+  toggleNavbar() {
+    this.navBurger.nativeElement.classList.toggle('is-active');
+    this.navMenu.nativeElement.classList.toggle('is-active');
+  }
   constructor() {}
 
   ngOnInit(): void {}
