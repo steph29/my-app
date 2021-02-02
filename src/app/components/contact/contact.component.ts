320093de29d7eb8df43e6e219c438ed2a8e00b5d
stephane <s.verardo29@gmail.com>
2021-02-02 21:12:16 +0100

    alert save work

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -10,31 +10,36 @@ import { NgbAlertConfig } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ContactComponent implements OnInit {
   alert: boolean = false;
+  bool: boolean = false;
 
   constructor() {}
 
+  ngOnInit(): void {}
+
   public sendEmail(event: Event) {
     event.preventDefault();
-    emailjs
-      .sendForm(
-        'service_adysozh',
-        'template_9on6n8k',
-        event.target as HTMLFormElement,
-        'user_1cpdEQHngg0lEUgm4n1Ul'
-      )
-      .then(
-        (result: EmailJSResponseStatus) => {
-          console.log(result.text);
-          this.alert = true;
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    if (this.bool) {
+      console.log('success');
+    } else {
+      emailjs
+        .sendForm(
+          'service_adysozh',
+          'template_9on6n8k',
+          event.target as HTMLFormElement,
+          'user_1cpdEQHngg0lEUgm4n1Ul'
+        )
+        .then(
+          (result: EmailJSResponseStatus) => {
+            console.log(result.text);
+            this.alert = true;
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    }
   }
   closeAlert() {
     this.alert = false;
   }
-
-  ngOnInit() {}
 }
