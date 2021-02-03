4b4556958f4768f87eae8c3457d2eafb7afa5086
Stephane Verardo <s.verardo29@gmail.com>
2021-02-03 08:12:20 +0100

    fix bugs

--- src/app/components/contact/dist/contact.component.js
+++ src/app/components/contact/dist/contact.component.js
@@ -9,38 +9,51 @@ exports.__esModule = true;
 exports.ContactComponent = void 0;
 var core_1 = require("@angular/core");
 var emailjs_com_1 = require("emailjs-com");
-var dialog_1 = require("@angular/material/dialog");
-var dialog_component_1 = require("./dialog/dialog.component");
 var ng_bootstrap_1 = require("@ng-bootstrap/ng-bootstrap");
 var ContactComponent = /** @class */ (function () {
-    function ContactComponent(dialog) {
-        this.dialog = dialog;
+    function ContactComponent() {
         this.alert = false;
         this.alertWrong = false;
+        this.isValidFormSubmitted = false;
     }
-    ContactComponent.prototype.openDialog = function () {
-        var dialogConfig = new dialog_1.MatDialogConfig();
-        var dialogRef = this.dialog.open(dialog_component_1.DialogComponent, {
-            width: '450px',
-            height: '200px'
-        });
-        dialogRef.afterClosed().subscribe(function (result) {
-            console.log("Dialog result: " + result);
-        });
+    ContactComponent.prototype.onFormSubmit = function (userForm) {
+        console.log(userForm);
+    };
+    ContactComponent.prototype.resetUserForm = function (userForm) {
+        userForm.resetForm();
+    };
+    ContactComponent.prototype.onFormSubmit2 = function (form) {
+        this.isValidFormSubmitted = false;
+        if (form.valid) {
+            this.isValidFormSubmitted = true;
+        }
+        else {
+            return;
+        }
+        var userName = form.controls['name'].value;
     };
     ContactComponent.prototype.ngOnInit = function () { };
-    ContactComponent.prototype.sendEmail = function (e) {
+    ContactComponent.prototype.validation = function (data, event) {
         var _this = this;
-        e.preventDefault();
-        emailjs_com_1["default"]
-            .sendForm('service_adysozh', 'template_9on6n8k', e.target, 'user_1cpdEQHngg0lEUgm4n1Ul')
-            .then(function (result) {
-            console.log(result.text);
-            _this.alert = true;
-        }, function (error) {
-            console.log(error.text);
-        });
+        if (data.from_name == '' || data.user_email == ' ' || data.message == '') {
+            this.alertWrong = true;
+            this.alert = false;
+        }
+        else {
+            this.alertWrong = false;
+            this.alert = true;
+            event.preventDefault();
+            emailjs_com_1["default"]
+                .sendForm('service_adysozh', 'template_9on6n8k', event.target, 'user_1cpdEQHngg0lEUgm4n1Ul')
+                .then(function (result) {
+                console.log(result.text);
+                _this.alert = true;
+            }, function (error) {
+                console.log(error.text);
+            });
+        }
     };
+    ContactComponent.prototype.sendEmail = function (event) { };
     ContactComponent.prototype.closeAlert = function () {
         this.alert = false;
     };
