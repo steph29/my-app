60331d7bb78851db87908e41f5acd4fb83f8cc5f
Stephane Verardo <s.verardo29@gmail.com>
2021-04-06 18:10:35 +0200

    Adjustment of Team page

--- src/app/components/passions/passions.component.ts
+++ src/app/components/passions/passions.component.ts
@@ -12,24 +12,29 @@ export class PassionsComponent implements OnInit {
       contentId: 0,
       contentTitle: "l'aéronautique",
       contentText:
-        'Passionné depuis tout petit par tout ce qui vole dans le ciel, ',
+        "Passionné depuis tout petit par tout ce qui vole dans le ciel, j'en ai fait mon métier. 10 années durant, j'ai été navigateur aérien au sein de la Marine Nationale. C'est un métier passionnant, rigoureux, et très enrichissant. ",
+      contentIcon: '/assets/images/fly.svg',
     },
     {
       contentId: 1,
       contentTitle: 'la course à pied',
       contentText:
-        "Pour les sites de Terre en Vie, j'ai utilisé MySQL. Utilisation des clés externes, sessions, page login / password sécurisée, page mon compte extractant les créneaux de bénévolats de fonction de la personne qui se connecte, mailing. J'ai également une formation sur MongoDB. Coté langage, j'ai une bonne connaissance de PHP, mais j'ai une appétance pour NodeJS et ExpressJS. Découverte de Symfony 4.",
+        "Ben oui, faut bien compenser les bons apéros ! Adepte des petites courses entre 10 et 15 km, j'essaie de courir en moyenne 1 à 2 fois par semaine. Le plus dur? c'est de se lancer, mais une fois parti !",
+      contentIcon: '/assets/images/fly.svg',
     },
     {
       contentId: 2,
       contentTitle: 'la vie',
-      contentText: '',
+      contentText:
+        "Que ce soit entre copains autour d'une bière locale, avec ma chérie dans un hamac au bord de la mer, j'aime profiter de chaque instant. ",
+      contentIcon: '/assets/images/fly.svg',
     },
     {
       contentId: 3,
       contentTitle: "l'écologie",
       contentText:
-        "Engagé de la première heure, je m'investi dans les associations comme Terre en Vie, pour défendre l'écologie et l'environnement. Directeur de l'entreprise Baladoù, nous avons créé un carnet de randonnées itinérant pour découvrir le pays de Vannes autrement",
+        "Engagé de la première heure, je m'investi dans les associations comme Terre en Vie, pour défendre l'écologie et l'environnement. Directeur de l'entreprise Baladoù, nous avons créé un carnet de randonnées itinérant pour découvrir le pays de Vannes autrement. Également bénévoles dans une épicerie associative, je donne régulièrement de mon temps pour les valeurs que je souhaite défendre : l'environnement, le local et la partage. Si vous vous reconnaissez dans ces mots, nou sommes fait pour travailler ensemble. ",
+      contentIcon: '/assets/images/fly.svg',
     },
   ];
   constructor() {}
