5f21eae37a92e56d5660f6112b7973698614e5b3
Stephane Verardo <s.verardo29@gmail.com>
2021-10-05 20:56:48 +0200

    change favicon, few text

--- src/app/components/passions/passions.component.ts
+++ src/app/components/passions/passions.component.ts
@@ -19,7 +19,7 @@ export class PassionsComponent implements OnInit {
       contentId: 1,
       contentTitle: 'la course à pied',
       contentText:
-        "Ben oui, faut bien compenser les bons apéros ! Adepte des petites courses entre 10 et 15 km, j'essaie de courir en moyenne 1 à 2 fois par semaine. Le plus dur? c'est de se lancer, mais une fois parti !",
+        "Ben oui, faut bien compenser les bons apéros ! Adepte des petites courses entre 10 et 15 km, j'essaie de courir en moyenne 1 à 2 fois par semaine. Le plus dur? c'est de se lancer, mais une fois parti, rien ne m'arrête !",
       contentIcon: '/assets/images/sneakers.png',
     },
     {
@@ -33,7 +33,7 @@ export class PassionsComponent implements OnInit {
       contentId: 3,
       contentTitle: "l'écologie",
       contentText:
-        "Engagé de la première heure, je m'investi dans les associations comme Terre en Vie, pour défendre l'écologie et l'environnement. Directeur de l'entreprise Baladoù, nous avons créé un carnet de randonnées itinérant pour découvrir le pays de Vannes autrement. Également bénévoles dans une épicerie associative, je donne régulièrement de mon temps pour les valeurs que je souhaite défendre : l'environnement, le local et la partage. Si vous vous reconnaissez dans ces mots, nou sommes fait pour travailler ensemble. ",
+        "Engagé de la première heure, je m'investi dans les associations comme Terre en Vie, pour défendre l'écologie et l'environnement. Directeur de l'entreprise Baladoù, nous avons créé un carnet de randonnées itinérant pour découvrir le pays de Vannes autrement. Également bénévoles dans une épicerie associative, je donne régulièrement de mon temps pour les valeurs que je souhaite défendre : l'environnement, le local et le partage. Si vous vous reconnaissez dans ces mots, nou sommes fait pour travailler ensemble. ",
       contentIcon: '/assets/images/recycle.png',
     },
   ];
